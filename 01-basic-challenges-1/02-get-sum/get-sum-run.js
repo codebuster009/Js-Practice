ce500b717a128842a15338308bf0243677812ae1
codebuster009 <kartavyasharmajs@gmail.com>
2023-11-06 14:46:23 +0530

    practice

---
+++ 01-basic-challenges-1/02-get-sum/get-sum-run.js
@@ -0,0 +1,5 @@
+const getSum = require('./get-sum');
+
+const result = getSum(9, 9);
+
+console.log(result);
---
+++ 01-basic-challenges-1/03-calculator/calculator-run.js
@@ -0,0 +1,5 @@
+const calculator = require('./calculator');
+
+const result = calculator(1, 9, '-');
+
+console.log(result);
