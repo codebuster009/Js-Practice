f6306933eca228be32605d7258877b1bdd01b6cf
kartavyaKloud <kartavya@kloudone.com>
2023-11-28 23:19:35 +0530

    Trying to Find First Non Repeating Character

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,15 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  let array = str.split("");
+  let uniqueCharacters = [];
+  let repeatedCharacter = [];
+  for (let i = 0; i < array.length; i++) {
+    if (uniqueCharacters.includes(array[i])) {
+      repeatedCharacter.push(array[i]);
+    } else {
+      uniqueCharacters.push(array[i]);
+    }
+  }
+  return uniqueCharacters[0]
+}
 
 module.exports = findFirstNonRepeatingCharacter;
