d1abec5b1d2d398e1fe1a9cbc2aba54a7034ec97
kartavyaKloud <kartavya@kloudone.com>
2023-12-02 14:36:00 +0530

    Using For Loop to Calculate Total Sales Tax

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,10 +1,25 @@
-function calculateTotalSalesWithTax(arr , tax) {
-    const totalPrice = arr[0].price * arr[0].quantity + arr[1].price * arr[1].quantity + arr[2].price * arr[2].quantity 
-    console.log(totalPrice)
-    const taxRate = tax / 100
-    const totalSale = totalPrice + taxRate
-    console.log(totalSale)
+function calculateTotalSalesWithTax(arr , tax) { 
+    let taxRate = tax/100
+    let totalPrice = 0
+    for(let i = 0; i < arr.length ; i++) {
+    totalPrice +=  arr[i].price * arr[i].quantity
+    }
+    saleTax =   totalPrice * tax/100
+    total = saleTax + totalPrice
 
+    let totalSalesWithTax = `${total}(${totalPrice} + ${tax}% tax)`
+    return totalSalesWithTax
 }
 
 module.exports = calculateTotalSalesWithTax;
+
+
+//Ineffective way
+// function calculateTotalSalesWithTax(arr , tax) {
+//     const totalPrice = arr[0].price * arr[0].quantity + arr[1].price * arr[1].quantity + arr[2].price * arr[2].quantity 
+//     console.log(totalPrice)
+//     const taxRate = tax / 100
+//     const totalSale = totalPrice + taxRate
+//     console.log(totalSale)
+
+// }
