037c97e7bf60ee5c163e955eca60fb889efe1964
kartavyaKloud <kartavya@kloudone.com>
2023-12-03 13:42:33 +0530

    Able to calculate the Total Price including tax using other way

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales-run.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales-run.js
@@ -1,11 +1,12 @@
 const calculateTotalSalesWithTax = require('./calculate-total-sales.js');
 
 const products = [
-  { name: 'Apple', price: 0.5, quantity: 10 },
-  { name: 'Banana', price: 0.3, quantity: 20 },
-  { name: 'Orange', price: 0.6, quantity: 15 },
+    { name: 'Chocolate', price: 2.5, quantity: 5 },
+    { name: 'Chips', price: 1.2, quantity: 10 },
+    { name: 'Soda', price: 1.0, quantity: 8 },
+    { name: 'Candy', price: 0.5, quantity: 15 },
 ];
 
-const result = calculateTotalSalesWithTax(products, 8);
+const result = calculateTotalSalesWithTax(products, 5);
 
 console.log(result);
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,25 +1,43 @@
-function calculateTotalSalesWithTax(arr , tax) { 
-    let taxRate = tax/100
-    let totalPrice = 0
-    for(let i = 0; i < arr.length ; i++) {
-    totalPrice +=  arr[i].price * arr[i].quantity
-    }
-    saleTax =   totalPrice * tax/100
-    total = saleTax + totalPrice
+//Always try first to solve with the loop approach and than  make it efficient afterwards
+//If I would have used Map Reduce both it would have been expensive process and internally both use for loop and we would be using two times instead of one 
 
-    let totalSalesWithTax = `${total}(${totalPrice} + ${tax}% tax)`
-    return totalSalesWithTax
+function calculateTotalSalesWithTax(prodArr, tax) {
+  let totalPriceBeforeTax = prodArr.reduce((totalPrice, item) => {
+    totalPrice += item.price * item.quantity;
+
+    return totalPrice;
+  }, 0);
+
+  let taxCost = (totalPriceBeforeTax * tax) / 100;
+
+  let totalPriceAfterTax = totalPriceBeforeTax + taxCost;
+
+  return `${totalPriceAfterTax}(${totalPriceBeforeTax} + ${tax}%)`;
 }
 
 module.exports = calculateTotalSalesWithTax;
-
-
 //Ineffective way
-// function calculateTotalSalesWithTax(arr , tax) {
-//     const totalPrice = arr[0].price * arr[0].quantity + arr[1].price * arr[1].quantity + arr[2].price * arr[2].quantity 
+// function caelculateTotalSalesWithTax(arr , tax) {
+//     const totalPrice = arr[0].price * arr[0].quantity + arr[1].price * arr[1].quantity + arr[2].price * arr[2].quantity
 //     console.log(totalPrice)
 //     const taxRate = tax / 100
 //     const totalSale = totalPrice + taxRate
 //     console.log(totalSale)
 
+// }
+
+
+//Effective Way
+// // Loop Method
+// function calculateTotalSalesWithTax(arr , tax) {
+//     let taxRate = tax/100
+//     let totalPrice = 0
+//     for(let i = 0; i < arr.length ; i++) {
+//     totalPrice +=  arr[i].price * arr[i].quantity
+//     }
+//     saleTax =   totalPrice * tax/100
+//     total = saleTax + totalPrice
+
+//     let totalSalesWithTax = `${total}(${totalPrice} + ${tax}% tax)`
+//     return totalSalesWithTax
 // }
