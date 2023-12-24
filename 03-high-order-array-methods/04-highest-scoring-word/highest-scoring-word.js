3a162ccdda803bbf3080f22d9ecd19b5d8407bab
kartavyaKloud <kartavya@kloudone.com>
2023-12-24 23:39:58 +0530

    Trying to flatten the array that is more than one deep

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,4 +1,6 @@
 function highestScoringWord(str) {
+  let arrayOfScores = [];
+  let sum = 0
   let alphabets = [
     'a', 'b', 'c', 'd', 'e', 'f',
     'g', 'h', 'i', 'j', 'k', 'l',
@@ -6,11 +8,43 @@ function highestScoringWord(str) {
     's', 't', 'u', 'v', 'w', 'x',
     'y', 'z'
   ]
-  let wordsArr = str.toLowerCase().split("");
+  let wordsArr = str.toLowerCase().split(" ");
   console.log(wordsArr);
   for (let i = 0; i < wordsArr.length; i++) {
-     word = wordsArr[i].join(" ")
-     console.log(alphabets.indexOf(word))
+    char = wordsArr[i].split("") 
+    console.log(char)
+    for(let char1 of wordsArr[i])
+    for(let j = i ; j < char.length ; j++){
+    sum+= alphabets.indexOf(char[i])
+    console.log(sum)
+    }
   }
 }
 module.exports = highestScoringWord;
+
+
+// function highestScoringWord(str) {
+//   let arrayOfScores = [];
+//   let sum = 0
+//   let alphabets = [
+//     'a', 'b', 'c', 'd', 'e', 'f',
+//     'g', 'h', 'i', 'j', 'k', 'l',
+//     'm', 'n', 'o', 'p', 'q', 'r',
+//     's', 't', 'u', 'v', 'w', 'x',
+//     'y', 'z'
+//   ]
+//   let wordsArr = str.toLowerCase().split(" ");
+//   console.log(wordsArr);
+//   for (let i = 0; i < wordsArr.length; i++) {
+//     char = wordsArr[i].split("")
+//     console.log(char)
+//    for(let j = i ; j < char.length ; j++){
+//    sum+= alphabets.indexOf(char[i]) 
+//  let obj = {Score : sum}
+//  arrayOfScores.push(obj)
+//     console.log(sum)
+//     }
+//   }
+//   console.log(arrayOfScores)
+// }
+// module.exports = highestScoringWord;
---
+++ 03-high-order-array-methods/Extras/ReadMe.md
@@ -0,0 +1,4 @@
+Question 1: Flat the below Array
+    const arr = [[1,2,3] , [4,5,6] , [7,8,9]] 
+
+    Output:[1,2,3,4,5,6,7,8,9]
